5b1a2f88e2a368d7a199ccb35165398737e103b7
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 11:31:58 +0200

    add failing test for currency statistics calculation

---
+++ CurrencyApp.Tests/CurrencyServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using CurrencyApp.Application.DTOs;
+using CurrencyApp.Application.Interfaces;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+
+namespace CurrencyApp.Tests
+{
+    public class CurrencyServiceTests
+    {
+        private Mock<ICurrencyProvider> _providerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _providerMock = new Mock<ICurrencyProvider>();
+        }
+
+        [Test]
+        public async Task Should_Calculate_min_max_avg_correctly()
+        {
+            var data = new List<CurrencyRateDto>() {
+                new() { Date = DateTime.Today, Rate = 4 },
+                new() { Date = DateTime.Today, Rate = 5 },
+                new() { Date = DateTime.Today, Rate = 6 }
+            };
+
+            _providerMock
+                .Setup(x => x.GetRatesAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+                .ReturnsAsync(data);
+
+            var service = new CurrencyService(_providerMock.Object);
+
+            var result = await service.GetCurrencyStatsAsync("EUR", "PLN", DateTime.Today, DateTime.Today);
+
+            result.Min.Should().Be(4);
+            result.Max.Should().Be(6);
+            result.Avg.Should().Be(5);
+        }
+    }
+}
