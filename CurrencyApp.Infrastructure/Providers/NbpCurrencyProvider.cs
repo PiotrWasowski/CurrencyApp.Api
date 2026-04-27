3a68bb050f76e5f81bcbdfa48f5455771d43a533
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 10:58:32 +0200

    implement NBP currency provider

---
+++ CurrencyApp.Infrastructure/Providers/NbpCurrencyProvider.cs
@@ -0,0 +1,34 @@
+﻿using CurrencyApp.Application.DTOs;
+using CurrencyApp.Application.Interfaces;
+using System.Net.Http.Json;
+
+namespace CurrencyApp.Infrastructure.Providers
+{
+    public class NbpCurrencyProvider: ICurrencyProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public NbpCurrencyProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<CurrencyRateDto>> GetRatesAsync(string from, string to, DateTime fromDate, DateTime toDate)
+        {
+            var url = $"exchangerates/rates/A/{from}/{fromDate:yyyy-MM-dd}/{toDate:yyyy-MM-dd}/";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+                throw new Exception("NBP API error");
+
+            var data = await response.Content.ReadFromJsonAsync<NbpResponse>();
+
+            return data.rates.Select(r => new CurrencyRateDto
+            {
+                Date = r.effectiveDate,
+                Rate = r.mid
+            }).ToList();
+        }
+    }
+}
---
+++ CurrencyApp.Infrastructure/Providers/NbpResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CurrencyApp.Infrastructure.Providers
+{
+    public class NbpResponse
+    {
+        public List<NbpRate> rates { get; set; }
+    }
+
+    public class NbpRate
+    {
+        public DateTime effectiveDate { get; set; }
+        public decimal mid { get; set; }
+    }
+}
