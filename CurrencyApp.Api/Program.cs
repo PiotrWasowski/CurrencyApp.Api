cefcb7b958ccf111ac4a0db23fa3d1f15c804828
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 11:01:05 +0200

    register NBP provider with HttpClient in DI

--- CurrencyApp.Api/Program.cs
+++ CurrencyApp.Api/Program.cs
@@ -1,6 +1,13 @@
+using CurrencyApp.Application.Interfaces;
+using CurrencyApp.Infrastructure.Providers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<ICurrencyProvider, NbpCurrencyProvider>(client =>
+{
+    client.BaseAddress = new Uri("https://api.nbp.pl/api/");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
