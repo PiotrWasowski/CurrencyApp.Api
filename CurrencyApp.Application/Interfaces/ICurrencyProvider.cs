16ec20c596057b0bfeb1f01cba017d280931cb2c
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 14:54:54 +0200

    add method for fetching available currencies in provider interface

--- CurrencyApp.Application/Interfaces/ICurrencyProvider.cs
+++ CurrencyApp.Application/Interfaces/ICurrencyProvider.cs
@@ -5,5 +5,6 @@ namespace CurrencyApp.Application.Interfaces
     public interface ICurrencyProvider
     {
         Task<List<CurrencyRateDto>> GetRatesAsync(string from, string to, DateTime fromDate, DateTime toDate);
+        Task<List<CurrencyDto>> GetCurrenciesAsync();
     }
 }
