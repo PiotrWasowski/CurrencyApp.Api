5caa8ce9e1e6277afa61506b3f94cbd8e661dfc7
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 11:16:56 +0200

    add currency stats DTO

---
+++ CurrencyApp.Application/DTOs/CurrencyStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyApp.Application.DTOs
+{
+    public class CurrencyStatsDto
+    {
+        public decimal Min { get; set; }
+        public decimal Max { get; set; }
+        public decimal Avg { get; set; }
+        public List<CurrencyRateDto> Rates { get; set; }
+    }
+}
