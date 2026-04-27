68ba260546beee7ed8cfa2a6ea4a5f9566b8defc
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-27 14:53:48 +0200

    add currency DTO for dropdown display

---
+++ CurrencyApp.Application/DTOs/CurrencyDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyApp.Application.DTOs
+{
+    public class CurrencyDto
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public string Display => $"{Code} - {Name}";
+    }
+}
