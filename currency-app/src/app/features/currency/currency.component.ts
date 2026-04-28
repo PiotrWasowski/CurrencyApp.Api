8cfdd9931e0fa2e06fa6b547ae3279ecb0d9e977
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-28 13:35:01 +0200

    add error handle for get rates

--- currency-app/src/app/features/currency/currency.component.ts
+++ currency-app/src/app/features/currency/currency.component.ts
@@ -1,7 +1,10 @@
+import * as TestReporters from 'node:test/reporters';
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from './currency.service';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Observable, of } from 'rxjs';
+import { tap, catchError, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-currency',
@@ -43,19 +46,40 @@ export class CurrencyComponent implements OnInit {
     });
   }
 
+  result$!: Observable<any>;
+
   submit() {
     this.loading = true;
     this.error = '';
+    this.result = null;
 
-    this.service.getRates(this.form.value).subscribe({
-      next: res => {
-        this.result = res;
+    this.result$ = this.service.getRates(this.form.value).pipe(
+      tap(res => {
+        if (!res) {
+          this.error = 'Brak danych dla wybranego zakresu';
+        }
+        else { 
+          this.error = '';
+        }
+      }),
+      catchError(err => {
+        this.handleError(err);
+        return of(null); 
+      }),
+      finalize(() => {
         this.loading = false;
-      },
-      error: () => {
-        this.error = 'Błąd pobierania danych';
-        this.loading = false;
-      }
-    });
+      })
+    );
+  }
+
+  handleError(error: any) {
+    console.log(error.status);
+    if (error.status === 0) {
+      this.error = 'Brak połączenia z serwerem';
+    } else if (error.status === 400) {
+      this.error = error.error || 'Nieprawidłowe dane';
+    }  else {
+      this.error = 'Błąd serwera';
+    }
   }
 }
