15a23ed1ece228379f7434e6e584cc8119e35651
WASOWSKI_DELL\wasol <wasol1980@gmail.com>
2026-04-29 09:19:12 +0200

    add angular materials

--- currency-app/src/app/app.config.ts
+++ currency-app/src/app/app.config.ts
@@ -1,14 +1,32 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { LOCALE_ID } from '@angular/core';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'yyyy-MM-dd',
+  },
+  display: {
+    dateInput: 'yyyy.MM.dd',
+    monthYearLabel: 'MMM yyyy',
+    dateA11yLabel: 'yyyy-MM-dd',
+    monthYearA11yLabel: 'MMMM yyyy',
+  },
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
+    provideNativeDateAdapter(),
+    { provide: LOCALE_ID, useValue: 'pl-PL' },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
   ]
 };
+
--- currency-app/src/app/features/currency/currency.component.ts
+++ currency-app/src/app/features/currency/currency.component.ts
@@ -1,72 +1,117 @@
-import * as TestReporters from 'node:test/reporters';
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from './currency.service';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, FormGroup, AbstractControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Observable, of } from 'rxjs';
 import { tap, catchError, finalize } from 'rxjs/operators';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-currency',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule
+  ],
   templateUrl: './currency.component.html'
 })
 export class CurrencyComponent implements OnInit {
-  form: any;
+
+  form: FormGroup;
+
+  result: any;
+  loading = false;
+  error = '';
+
+  currencies$!: Observable<any[]>;
+  result$!: Observable<any>;
+
   constructor(
     private service: CurrencyService,
     private fb: FormBuilder
   ) {
     this.form = this.fb.group({
-      apiType: ['Nbp'],
-      from: [''],
-      to: [''],
-      fromDate: [''],
-      toDate: ['']
-    });
-  }
-    
-  result: any;
-  loading = false;
-  error = '';
+      apiType: ['Nbp', Validators.required],
+      from: ['', Validators.required],
+      to: ['', Validators.required],
 
-  
+      fromDate: ['', [Validators.required, this.futureDateValidator.bind(this)]],
+      toDate: ['', [Validators.required, this.futureDateValidator.bind(this)]]
+    }, { validators: this.dateRangeValidator });
+  }
+
   ngOnInit() {
     this.loadCurrencies();
   }
 
-  currencies$!: Observable<any[]>;
+  dateRangeValidator(group: FormGroup) {
+    const from = group.get('fromDate')?.value;
+    const to = group.get('toDate')?.value;
+
+    if (from && to && new Date(from) > new Date(to)) {
+      return { dateRange: true };
+    }
+
+    return null;
+  }
+
+  futureDateValidator(control: AbstractControl) {
+    if (control.value && new Date(control.value) > new Date()) {
+      return { futureDate: true };
+    }
+    return null;
+  }
 
   loadCurrencies() {
     const api = this.form.value.apiType!;
+
     this.currencies$ = this.service.getCurrencies(api).pipe(
-      catchError(err => {
+      catchError(() => {
         this.error = 'Nie udało się pobrać listy walut';
         return of([]);
       })
-    );;
+    );
   }
 
-  result$!: Observable<any>;
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
 
   submit() {
     this.loading = true;
     this.error = '';
     this.result = null;
 
-    this.result$ = this.service.getRates(this.form.value).pipe(
+    const formValue = this.form.value;
+
+    const request = {
+      ...formValue,
+      fromDate: this.formatDate(formValue.fromDate),
+      toDate: this.formatDate(formValue.toDate)
+    };
+
+    this.result$ = this.service.getRates(request).pipe(
       tap(res => {
-        if (!res) {
-          this.error = 'Brak danych dla wybranego zakresu';
-        }
-        else { 
-          this.error = '';
-        }
+        this.error = res ? '' : 'Brak danych dla wybranego zakresu';
       }),
       catchError(err => {
         this.handleError(err);
-        return of(null); 
+        return of(null);
       }),
       finalize(() => {
         this.loading = false;
@@ -76,11 +121,12 @@ export class CurrencyComponent implements OnInit {
 
   handleError(error: any) {
     console.log(error.status);
+
     if (error.status === 0) {
       this.error = 'Brak połączenia z serwerem';
     } else if (error.status === 400) {
       this.error = error.error || 'Nieprawidłowe dane';
-    }  else {
+    } else {
       this.error = 'Błąd serwera';
     }
   }
